ebe9cf0e37e1f288666a357cf3b9573c9097187c
aman <amanabhaysingh@gmail.com>
2026-01-12 10:03:39 +0530

    fix: update Celery command in docker-compose and configure broker URL from environment variable

--- server/app/tasks/celery_app.py
+++ server/app/tasks/celery_app.py
@@ -1,6 +1,8 @@
+import os
 from celery import Celery
 
-celery_app = Celery('tasks', broker='pyamqp://guest@localhost//')
+broker_url = os.getenv('CELERY_BROKER_URL', 'pyamqp://guest:guest@localhost//')
+celery_app = Celery('tasks', broker=broker_url)
 
 @celery_app.task(name="app.tasks.add")
 def add(x, y):
